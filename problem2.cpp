8df43904b208c4e806b769e8a6d0ab95b0d38d64
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-11 14:01:25 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-// int main() {
-//     int m, n;
-//     cin >> m >> n;
+#define rp(i, a, b) for(int i = a; i < b; i++)
 
-//     vector<vector<int>> matrix(m, vector<int>(n));
-//     for (int i = 0; i < m; ++i)
-//         for (int j = 0; j < n; ++j)
-//             cin >> matrix[i][j];
+bool isa(const vector<vector<int>>& v, int m, int n) {
+    double c = 0;
+    rp(j, 0, n){    c += v[0][j];}
+    c /= n;
+    rp(i, 1, m){
+        double ra = 0;
+        rp(j, 0, n){    ra += v[i][j];}
+        ra /= n;
+        if(ra != c){    return false;}
+    }
+    rp(j, 0, n){
+        double ca = 0;
+        rp(i, 0, m){    ca += v[i][j];}
+        ca /= m;
+        if(ca != c){    return false;}
+    }
+    return true;
+}
 
-//     double rowSum = 0, colSum = 0;
-    
-//     for (int i = 0; i < m; ++i) {
-//         double rowTotal = 0;
-//         for (int j = 0; j < n; ++j)
-//             rowTotal += matrix[i][j];
-//         rowSum += rowTotal / n;
-//     }
-
-//     for (int j = 0; j < n; ++j) {
-//         double colTotal = 0;
-//         for (int i = 0; i < m; ++i)
-//             colTotal += matrix[i][j];
-//         colSum += colTotal / m;
-//     }
-
-//     cout << (rowSum == colSum ? "YES" : "NO") << endl;
-
-//     return 0;
-// }
+int main(){
+    int m, n;
+    cin >> m >> n;
+    vector<vector<int>> v(m, vector<int>(n));
+    rp(i, 0, m){    rp(j, 0, n){    cin >> v[i][j]; }   }
+    cout << (isa(v, m, n) ? "YES" : "NO") << endl;
+    return 0;
+}
